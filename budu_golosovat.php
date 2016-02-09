d776e43c8e92297b47063d3530dc61ca1094fb5f
mirtlbu <bubuzoglo@gmail.com>
2016-02-09 13:59:22 +0300

    budu golosovat page start

---
+++ budu_golosovat.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Gudkov</title>
+        <link href='https://fonts.googleapis.com/css?family=Ubuntu:500,400&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/social-likes_birman.css">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+
+        <div class="header">
+            <div class="container">
+                <div class="flexbox">
+                    <div class="flexbox__item">
+                        <div class="logo">
+                            <div class="logo__img"></div>
+                            <div class="logo__text">Дмитрий Гудков</div>
+                        </div>
+                        <a href="#" class="breadcrumbs">
+                            <div class="breadcrumbs__image"></div>
+                            <div class="breadcrumbs__text">На главную</div>
+                        </a>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="header__directions">
+                            <span class="header__directions--left">
+                                <a href="#" class="directions__link">Хочу помогать</a>
+                            </span>
+                            <span class="header__directions--right">
+                                <a href="#" class="directions__link">Поддержать кампанию</a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="budu_golosovat">
+            <div class="container">
+                <div class="flexbox">
+                    <div class="info">
+                        <div class="info__title text-big">Я буду голосовать сам</div>
+                            <div class="info__text">
+                                <p>Нам нужна информация о вас, чтобы мы смогли пригласить вас на встречу с Дмитрием,
+                                 когда она пройдёт в вашем районе, прислать вам открытку и напомнить вам о голосовании.</p>
+                                 <p>Это правда важно — независимые кандидаты проигрывают выборы потому,
+                                    что наши избиратели не приходят на участок (а избиратель кандидата от власти будет приведён начальником).
+                                    Позвольте нам напомнить вам о выборах перед ними.</p>
+                            </div>
+                        </div>
+                        <div class="note">
+                            <div class="note__text">Голосовать могут только жители районов:</div>
+                            <div class="note__bold">Митино, Строгино, Покровское-Стрешнево, Хорошево-Мневники, Щукино, Южное Тушино</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="counter">
+                    <div class="container">
+                        <div class="counter__item">
+                            <div class="counter__num">4584</div>
+                            <div class="counter__text">голосов у&nbsp;нас есть</div>
+                        </div>
+                        <div class="counter__item">
+                            <div class="counter__num">/</div>
+                        </div>
+                        <div class="counter__item">
+                            <div class="counter__num">125 000</div>
+                            <div class="counter__text">нужно для&nbsp;победы</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <form class="form">
+                        <div class="form__row">
+                            <label for="surname" class="form__label">Фамилия,</label>
+                            <input id="surname" class="form__input" type="text" required></input>
+                        </div>
+                        <div class="form__row">
+                            <label for="name" class="form__label">имя,</label>
+                            <input id="name" class="form__input" type="text" required></input>
+                        </div>
+                        <div class="form__row">
+                            <label for="middlename" class="form__label">отчество</label>
+                            <input id="middlename" class="form__input" type="text" required></input>
+                        </div>
+                        <div class="form__row">
+                            <label for="street" class="form__label">Где прописан</label>
+                            <input id="street" class="form__input" type="text" required></input>
+                        </div>
+                        <div class="form__row">
+                            <label for="appartment" class="form__label"></label>
+                            <input id="appartment" class="form__input" type="text" required></input>
+                        </div>
+                        <div class="form__row">
+                            <label for="birthdate" class="form__label">Год рождения</label>
+                            <input id="birthdate" class="form__input" type="text" required></input>
+                        </div>
+                        <div class="form__row">
+                            <label for="social" class="form__label">Ваши соцсети</label>
+                            <input id="social" class="form__input" type="text" required></input>
+                        </div>
+                        <div class="form__row">
+                            <label for="phone" class="form__label">Телефон</label>
+                            <input id="phone" class="form__input" type="phone" required></input>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="container">
+                <div class="footer__text">Расскажи друзьям о кампании</div>
+                <div class="footer__social">
+                    <div class="social-likes">
+                        <div class="facebook" title="Поделиться ссылкой на Фейсбуке">Facebook</div>
+                        <div class="twitter" title="Поделиться ссылкой в Твиттере">Twitter</div>
+                        <div class="vkontakte" title="Поделиться ссылкой во Вконтакте">Вконтакте</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="js/social-likes.min.js"></script>
+        <script src="js/script.js" type="text/javascript"></script>
+
+    </body>
+</html>
