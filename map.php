9af574b7d0c0cff230726dbcb0009a9db6fc6245
mirtlbu <bubuzoglo@gmail.com>
2016-02-18 01:36:56 +0300

    map style and nabiraem page

--- map.php
+++ map.php
@@ -4,9 +4,9 @@ gdMap = {
   init: function(){
     gdMap.initMap();
   },
-  
+
   initMap: function(){
-    
+
     gdMap.map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: {lat: 55.76, lng: 37.64}
@@ -26,7 +26,31 @@ gdMap = {
 
 
 
-  
+
 <center>
   <div id="map"></div>
+  <div class="baloon">
+                <div class="baloon__title">
+                    <span class="baloon__num">100</span> из <span class="baloon__num">4100</span> помощников уже&nbsp;нашлись
+                </div>
+                <a href="" class="button button--blue">Хочу помогать</a>
+                <ul class="baloon__list">
+                    <li class="list__item">
+                        <div class="list__count"><span class="list__num">349</span></div>
+                        <div class="list__text">Дом полон сторонников</div>
+                    </li>
+                    <li class="list__item list__item--few">
+                        <div class="list__count"><span class="list__num">30</span></div>
+                        <div class="list__text">В этом доме нужны ещё люди</div>
+                    </li>
+                    <li class="list__item list__item--no">
+                        <div class="list__count"><span class="list__num"></span></div>
+                        <div class="list__text">Ещё нет сторонников</div>
+                    </li>
+                </ul>
+            </div>
+            <div class="baloon baloon--home">
+                <button class="button button--blue" type="button">Добавить информацию про дом</button>
+                <button class="button button--blue" type="button">Про жильцов</button>
+            </div>
 </center>
--- nabiraem.php
+++ nabiraem.php
@@ -21,144 +21,162 @@
         </div>
     </div>
     <div class="map">
-        <div id="map"></div>
-
-        <script>
-            gdMap = {
-              init: function(){
-                gdMap.initMap();
-              },
-
-              initMap: function(){
-
-                gdMap.map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 13,
-                center: {lat: 55.76, lng: 37.64}
-                });
-
-                gdMapAdmin.init();
-                google.maps.event.addListener(gdMap.map, 'idle', gdMap.showMarkers);
-
-
-                gdMap.drawRegions();
-              },
-
-            };
-</script>
-
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1QFnZAC1l-koq08KZi8tQilnvf_FbLGo&signed_in=true&callback=gdMap.init"></script>
+        <?php include "map.php";?>
         <div class="container">
-            <div class="baloon">
-                <div class="baloon__title">
-                    <span class="baloon__num">100</span> из <span class="baloon__num">4100</span> помощников уже&nbsp;нашлись
-                </div>
-                <a href="" class="button button--blue">Хочу помогать</a>
-                <ul class="baloon__list">
-                    <li class="list__item">
-                        <div class="list__count"><span class="list__num">349</span></div>
-                        <div class="list__text">Дом полон сторонников</div>
-                    </li>
-                    <li class="list__item list__item--few">
-                        <div class="list__count"><span class="list__num">30</span></div>
-                        <div class="list__text">В этом доме нужны ещё люди</div>
-                    </li>
-                    <li class="list__item list__item--no">
-                        <div class="list__count"><span class="list__num"></span></div>
-                        <div class="list__text">Ещё нет сторонников</div>
-                    </li>
-                </ul>
-            </div>
-            <div class="baloon baloon--home">
-                <button class="button button--blue" type="button">Добавить информацию про дом</button>
-                <button class="button button--blue" type="button">Про жильцов</button>
-            </div>
+            <div class="map__info text-big">Кликни на дом и расскажи о нём</div>
         </div>
     </div>
     <div class="container">
-        <form class="form">
-            <div class="form__row">
-                <label for="address" class="form__label">Адрес</label>
-                <input id="address" name="address" class="form__input form__input--long" type="text" required>
-            </div>
-            <div class="form__row form__row--lined">
-                <label for="middlename" class="form__label">№ квартиры</label>
-                <input id="middlename" name="middlename" class="form__input form__input--short" type="text" pattern="[0-9]+" required>
-            </div>
-            <div class="form__row">
-                <div class="form__title text-big">Жилец 1</div>
-            </div>
-            <div class="form__row form__row--cluster">
-                <label for="name" class="form__label">Имя,</label>
-                <input id="name" name="name" class="form__input form__input--top" type="text" pattern="[a-zA-Z,а-яА-Я]+" required>
-            </div>
-            <div class="form__row form__row--cluster">
-                <label for="middlename" class="form__label">отчество</label>
-                <input id="middlename" name="middlename" class="form__input form__input--middle" type="text" pattern="[a-zA-Z,а-яА-Я]+" required>
-            </div>
-            <div class="form__row">
-                <label for="surname" class="form__label">фамилия</label>
-                <input id="surname" name="surname" class="form__input form__input--bottom" type="text" pattern="[a-zA-Z,а-яА-Я]+" required>
-            </div>
-            <div class="form__row">
-                <label for="party" class="form__label">Какой партии симпатизирует</label>
-                <input id="party" name="party" class="form__input form__input--long" type="text" required>
-            </div>
-            <div class="form__row">
-                <label for="age" class="form__label">Возраст</label>
-                <input id="age" name="age" class="form__input form__input--short" type="text" pattern="[0-9]+" required>
-            </div>
-            <div class="form__row flexbox flexbox--start">
-                <label for="" class="form__label">Собирается ли на выборы</label>
-                <div class="form__radios">
+        <div class="flexbox">
+            <form class="form">
+                <div class="form__row">
+                    <label for="address" class="form__label">Адрес</label>
+                    <input id="address" name="address" class="form__input form__input--long" type="text" required>
+                </div>
+                <div class="form__row form__row--lined">
+                    <label for="middlename" class="form__label">№ квартиры</label>
+                    <input id="middlename" name="middlename" class="form__input form__input--short" type="text" pattern="[0-9]+" required>
+                </div>
+                <div class="tenant1" id="tenant1">
                     <div class="form__row">
-                        <input id="toelections1" name="toelections" value="yes" class="form__input form__input--hidden" type="radio" checked>
-                        <label for="toelections1" class="form__radio-circle">
-                            <span>Да</span>
-                        </label>
+                        <div class="form__title text-big">Жилец 1</div>
+                    </div>
+                    <div class="form__row form__row--cluster">
+                        <label for="name1" class="form__label">Имя,</label>
+                        <input id="name1" name="name1" class="form__input form__input--top" type="text" pattern="[a-zA-Z,а-яА-Я]+" required>
+                    </div>
+                    <div class="form__row form__row--cluster">
+                        <label for="middlename1" class="form__label">отчество</label>
+                        <input id="middlename1" name="middlename1" class="form__input form__input--middle" type="text" pattern="[a-zA-Z,а-яА-Я]+" required>
                     </div>
                     <div class="form__row">
-                        <input id="toelections2" name="toelections" value="no" class="form__input form__input--hidden" type="radio">
-                        <label for="toelections2" class="form__radio-circle">
-                            <span>Нет</span>
-                        </label>
+                        <label for="surname1" class="form__label">фамилия</label>
+                        <input id="surname1" name="surname1" class="form__input form__input--bottom" type="text" pattern="[a-zA-Z,а-яА-Я]+" required>
                     </div>
                     <div class="form__row">
-                        <input id="toelections3" name="toelections" value="unknown" class="form__input form__input--hidden" type="radio">
-                        <label for="toelections3" class="form__radio-circle">
-                            <span>Не удалось узнать</span>
-                        </label>
+                        <label for="party1" class="form__label">Какой партии симпатизирует</label>
+                        <input id="party1" name="party1" class="form__input form__input--long" type="text" required>
+                    </div>
+                    <div class="form__row">
+                        <label for="age1" class="form__label">Возраст</label>
+                        <input id="age1" name="age1" class="form__input form__input--short" type="text" pattern="[0-9]+" required>
+                    </div>
+                    <div class="form__row flexbox flexbox--start">
+                        <label for="" class="form__label">Собирается ли на выборы</label>
+                        <div class="form__radios">
+                            <div class="form__row">
+                                <input id="toelections-yes1" name="toelections1" value="yes" class="form__input form__input--hidden" type="radio" checked>
+                                <label for="toelections-yes1" class="form__radio-circle">
+                                    <span>Да</span>
+                                </label>
+                            </div>
+                            <div class="form__row">
+                                <input id="toelections-no1" name="toelections1" value="no" class="form__input form__input--hidden" type="radio">
+                                <label for="toelections-no1" class="form__radio-circle">
+                                    <span>Нет</span>
+                                </label>
+                            </div>
+                            <div class="form__row">
+                                <input id="toelections-unknown1" name="toelections1" value="unknown" class="form__input form__input--hidden" type="radio">
+                                <label for="toelections-unknown1" class="form__radio-circle">
+                                    <span>Не удалось узнать</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form__row">
+                        <label for="other1" class="form__label">Другое</label>
+                        <textarea id="other1" name="other1" class="form__textarea form__input--long"></textarea>
                     </div>
                 </div>
-            </div>
-            <div class="form__row">
-                <label for="other" class="form__label">Другое</label>
-                <textarea id="other" name="other" class="form__textarea form__input--long"></textarea>
-            </div>
-            <div class="form__row">
-                <div class="add">
-                    <div class="add__button form__radio-button">
-                        <div class="add__image"></div>
+                <div class="tenant2" id="tenant2">
+                    <div class="form__row">
+                        <div class="form__title text-big">Жилец 2</div>
                     </div>
-                    <div class="text-small add__text">Добавить жильца в квартиру</div>
-                </div>
-            </div>
-            <div class="form__row form__row--lined">
-                <div class="add">
-                    <div class="add__button form__radio-button">
-                        <div class="add__image"></div>
+                    <div class="form__row form__row--cluster">
+                        <label for="name2" class="form__label">Имя,</label>
+                        <input id="name2" name="name2" class="form__input form__input--top" type="text" pattern="[a-zA-Z,а-яА-Я]+" required>
                     </div>
-                    <div class="text-small add__text">Добавить eщё квартиру в доме</div>
+                    <div class="form__row form__row--cluster">
+                        <label for="middlename2" class="form__label">отчество</label>
+                        <input id="middlename2" name="middlename2" class="form__input form__input--middle" type="text" pattern="[a-zA-Z,а-яА-Я]+" required>
+                    </div>
+                    <div class="form__row">
+                        <label for="surname2" class="form__label">фамилия</label>
+                        <input id="surname2" name="surname2" class="form__input form__input--bottom" type="text" pattern="[a-zA-Z,а-яА-Я]+" required>
+                    </div>
+                    <div class="form__row">
+                        <label for="party2" class="form__label">Какой партии симпатизирует</label>
+                        <input id="party2" name="party2" class="form__input form__input--long" type="text" required>
+                    </div>
+                    <div class="form__row">
+                        <label for="age2" class="form__label">Возраст</label>
+                        <input id="age2" name="age2" class="form__input form__input--short" type="text" pattern="[0-9]+" required>
+                    </div>
+                    <div class="form__row flexbox flexbox--start">
+                        <label for="" class="form__label">Собирается ли на выборы</label>
+                        <div class="form__radios">
+                            <div class="form__row">
+                                <input id="toelections-yes2" name="toelections2" value="yes" class="form__input form__input--hidden" type="radio" checked>
+                                <label for="toelections-yes2" class="form__radio-circle">
+                                    <span>Да</span>
+                                </label>
+                            </div>
+                            <div class="form__row">
+                                <input id="toelections-no2" name="toelections2" value="no" class="form__input form__input--hidden" type="radio">
+                                <label for="toelections-no2" class="form__radio-circle">
+                                    <span>Нет</span>
+                                </label>
+                            </div>
+                            <div class="form__row">
+                                <input id="toelections-unknown2" name="toelections2" value="unknown" class="form__input form__input--hidden" type="radio">
+                                <label for="toelections-unknown2" class="form__radio-circle">
+                                    <span>Не удалось узнать</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form__row">
+                        <label for="other2" class="form__label">Другое</label>
+                        <textarea id="other2" name="other" class="form__textarea form__input--long"></textarea>
+                    </div>
                 </div>
-            </div>
-            <div class="form__row form__row--button form__row--lined">
-                <label class="form__label"></label>
-                <button class="button button--blue" type="submit">Отправить</button>
-                <div class="form__error text-small">
-                    <div>Нужно заполнить:</div>
-                    <div id="error-message">год рождения</div>
+                <div class="form__row">
+                    <div class="add" id="addman" onClick="addman()">
+                        <div class="add__button form__radio-button">
+                            <div class="add__image"></div>
+                        </div>
+                        <div class="text-small add__text">Добавить жильца в квартиру</div>
+                    </div>
                 </div>
+                <script type="text/javascript">
+                    function addman() {
+                        var tenant = document.getElementById('tenant2');
+                        tenant.style.display = 'block';
+                    }
+                </script>
+                <div class="form__row form__row--lined">
+                    <div class="add">
+                        <div class="add__button form__radio-button">
+                            <div class="add__image"></div>
+                        </div>
+                        <div class="text-small add__text">Добавить eщё квартиру в доме</div>
+                    </div>
+                </div>
+                <div class="form__row form__row--button form__row--lined">
+                    <label class="form__label"></label>
+                    <button class="button button--blue" type="submit">Отправить</button>
+                    <div class="form__error text-small">
+                        <div>Нужно заполнить:</div>
+                        <div id="error-message">год рождения</div>
+                    </div>
+                </div>
+            </form>
+            <div class="mailme">
+                Если у вас много другой нужной нам информации, отправьте её на почту  <a href="" class="text-link">shtab@gudkov.ru</a>
             </div>
-        </form>
+        </div>
+
         <form class="form">
             <div class="form__row">
                 <label for="address" class="form__label form__label--separated">Адрес</label>
