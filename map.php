619a6d671e2227618a0b0f9d606b7e36ca0a1747
Victor <viksem999@gmail.com>
2016-02-17 22:17:33 +0200

    Map added

--- map.php
+++ map.php
@@ -1,3 +1,32 @@
+<script>
+
+gdMap = {
+  init: function(){
+    gdMap.initMap();
+  },
+  
+  initMap: function(){
+    
+    gdMap.map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 55.76, lng: 37.64}
+    });
+
+
+  },
+
+};
+
+</script>
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1QFnZAC1l-koq08KZi8tQilnvf_FbLGo&signed_in=true&callback=gdMap.init"></script>
+
+
+
+
+
+
+  
 <center>
-    <div id="map"></div>
+  <div id="map"></div>
 </center>
