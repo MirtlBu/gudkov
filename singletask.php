cccc51e1badcc420c6ddf801ef180d194051341c
mirtlbu <bubuzoglo@gmail.com>
2016-02-19 23:23:37 +0300

    tasklist and singletask

---
+++ singletask.php
@@ -0,0 +1,51 @@
+<div class="content">
+    <?php include "dashboard.php"; ?>
+    <div class="container">
+        <a href="" class="breadcrumbs-content text-link">Все задания</a>
+        <div class="singletask">
+            <div class="text-bigger">Разнести газеты по подъездам</div>
+            <div class="flexbox">
+                <div class="flexbox__item steps">
+                    <div class="flexbox">
+                        <div class="text-date">Поставлено 26 февраля</div>
+                        <div class="text-date">Дедлайн 12 марта</div>
+                    </div>
+                    <div class="text-big steps__title">Шаги</div>
+                    <div class="steps__item">
+                        <div class="text-big">1</div>
+                        <div>Забрать газеты в штабе. Ул. Тверская, д 9. С 10 до 23 часов. Спросить Иру</div>
+                    </div>
+                    <div class="steps__item">
+                        <div class="text-big">2</div>
+                        <div>Разнести по подъездам в районе Митино в своём доме.</div>
+                    </div>
+                    <div class="steps__item">
+                        <div class="text-big">3</div>
+                        <div>На следующий день проверить, как там дела</div>
+                    </div>
+                    <div class="steps__item">
+                        <div class="text-big">4</div>
+                        <div>Доложить координатору обстановку</div>
+                    </div>
+                    <div class="steps__item">
+                        <div class="text-big">5</div>
+                        <div>Получить печеньки</div>
+                    </div>
+                    <div class="steps__item">
+                        <div class="text-big">3</div>
+                        <div>Пойти отоспаться</div>
+                    </div>
+                </div>
+                <div class="flexbox__item table">
+                    <div class="table__header flexbox">
+                        <div class="flex__item text-small">Координаторы (Всего 24)</div>
+                        <div class="flex__item text-small">
+                            <div>Сколько помощников выполнили</div>
+                            <div class="filter">Сначала худшие</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- tasklist.php
+++ tasklist.php
@@ -1,34 +1,34 @@
 <div class="content">
     <?php include "dashboard.php"; ?>
-    <div class="container tasklist">
+    <div class="container">
         <div class="tasklist">
             <div class="text-bigger">Список заданий</div>
             <a href="" class="tasklist__item">
                 <div href="" class="tasklist__title">Разнести газетки по подъездам</div>
                 <div class="flexbox">
-                    <div class="flexbox__item text-small">Поставлено 25 февраля</div>
-                    <div class="flexbox__item text-small">Дедлайн 5 марта</div>
+                    <div class="text-date">Поставлено 25 февраля</div>
+                    <div class="text-date">Дедлайн 5 марта</div>
                 </div>
             </a>
             <a href="" class="tasklist__item">
                 <div href="" class="tasklist__title">Убраться у дизайнера в квартире</div>
                 <div class="flexbox">
-                    <div class="flexbox__item text-small">Поставлено 15 февраля</div>
-                    <div class="flexbox__item text-small">Дедлайн 2 марта</div>
+                    <div class="text-date">Поставлено 15 февраля</div>
+                    <div class="text-date">Дедлайн 2 марта</div>
                 </div>
             </a>
             <a href="" class="tasklist__item">
                 <div href="" class="tasklist__title">Починить столы и стулья в офисе на Тверской 9</div>
                 <div class="flexbox">
-                    <div class="flexbox__item text-small">Поставлено 26 февраля</div>
-                    <div class="flexbox__item text-small">Дедлайн 12 марта</div>
+                    <div class="text-date">Поставлено 26 февраля</div>
+                    <div class="text-date">Дедлайн 12 марта</div>
                 </div>
             </a>
             <a href="" class="tasklist__item">
                 <div href="" class="tasklist__title">Поиграть в ФИФУ с Кацем</div>
                 <div class="flexbox">
-                    <div class="flexbox__item text-small">Поставлено 26 февраля</div>
-                    <div class="flexbox__item text-small">Дедлайн 12 марта</div>
+                    <div class="text-date">Поставлено 26 февраля</div>
+                    <div class="text-date">Дедлайн 12 марта</div>
                 </div>
             </a>
         </div>
