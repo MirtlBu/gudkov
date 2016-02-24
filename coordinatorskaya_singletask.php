58a554273b8448d9e43e9da830ba35177dbd418c
mirtlbu <bubuzoglo@gmail.com>
2016-02-24 20:21:52 +0300

    coordinatorskaya_singletask

--- coordinatorskaya_singletask.php
+++ coordinatorskaya_singletask.php
@@ -1,25 +1,36 @@
-<div class="content">
+<div class="content coordinatorskaya">
     <div class="dashboard">
-        <div class="container flexbox">
+        <div class="container flexbox flexbox--verticalmiddle">
             <div class="flexbox__item">
                 <div class="flexbox__container">
-                    <div class="dashboard__title">Админская</div>
+                    <div class="dashboard__title">Координаторская</div>
                     <div class="dashboard__tablo">
-                        <div class="dashboard__tasks">
-                            <a href="" class="text-link">Список заданий</a>
-                        </div>
-                        <div class="dashboard__errors">
-                            <a href="" class="text-link">Ошибки пользователей</a>
-                            <span>+34</span>
+                        <div class="dashboard__row">Валера, как прошёл день?</div>
+                        <div class="dashboard__rate">
+                            <button class="button button--small button--red">Хренова</button>
+                            <button class="button button--small button--blue">Нормально</button>
+                            <button class="button button--small button--green">Огонь!</button>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="flexbox__item">
                 <div class="flexbox__container">
-                    <button class="button button--blue" type="button">Добавить агитатора</button>
-                    <button class="button button--blue" type="button">Добавить координатора</button>
                     <button class="button button--blue" type="button">Добавить помощника</button>
+                    <div class="dashboard__row dashboard__bold">УИКи</div>
+                    <div class="flexbox">
+                        <div class="flexbox__item">
+                            <div class="dashboard__row">23534667</div>
+                            <div class="dashboard__row">23534667</div>
+                        </div>
+                        <div class="flexbox__item">
+                            <div class="dashboard__row">23534667</div>
+                            <div class="dashboard__row">23534667</div>
+                        </div>
+                        <div class="flexbox__item">
+                            <div class="dashboard__row">23534667</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
