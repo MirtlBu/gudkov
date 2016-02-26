d123c8b0639681440c2a4010569703c750496b18
mirtlbu <bubuzoglo@gmail.com>
2016-02-26 21:03:55 +0300

    add modal5

---
+++ modal5.php
@@ -0,0 +1,8 @@
+<div class="modal-wrap modal-wrap--active">
+    <div class="modal modal5">
+        <div class="modal__title">
+            <div class="text-bigger">Спасибо!</div>
+        </div>
+        <div class="icon-close"></div>
+    </div>
+</div>
