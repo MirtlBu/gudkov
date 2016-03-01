d6d2c98cdef17142ddf8b960fa8e68e9bf04c04b
mirtlbu <bubuzoglo@gmail.com>
2016-03-01 18:03:25 +0300

    pomoshnik_tasklist progressbar

--- pomoshnik_tasklist.php
+++ pomoshnik_tasklist.php
@@ -29,6 +29,67 @@
         </div>
     </div>
     <div class="container">
-
+        <div class="flexbox your__info">
+            <div class="flexbox__item form">
+                <div class="form__row">
+                    <div class="text-small text-small--light">Твой координатор</div>
+                    <div class="text-big">Виктория Черняховская</div>
+                    <div>+7 904 335-66-45</div>
+                </div>
+                <div class="form__row">
+                    <div class="text-small text-small--light">Твой основной участок</div>
+                    <div class="">ул. Народных гуляний, д.104, подъезд 2</div>
+                </div>
+            </div>
+            <div class="flexbox__item progressboard">
+                <div class="yourprogress">
+                    <div class="text-bold">Твой личный опыт</div>
+                    <div class="progressbar">
+                        <div class="progressbar-outer">
+                            <div class="progressbar-inner"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="campaignprogress">
+                    <div class="text-bold">Успешность кампании</div>
+                    <div class="progressbar">
+                        <div class="progressbar-outer">
+                            <div class="progressbar-inner"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="yourmedals">
+                    <div>Медали — 5 штук</div>
+                    <div class="flexbox">
+                        <div class="flexbox__item">
+                            <div class="medal">
+                                <img class="medal__img" src="/static/images/medal.png">
+                                <div class="medal__text text-small">За красивые глаза</div>
+                            </div>
+                            <div class="medal">
+                                <img class="medal__img" src="/static/images/medal.png">
+                                <div class="medal__text text-small">За активность</div>
+                            </div>
+                        </div>
+                        <div class="flexbox__item">
+                            <div class="medal">
+                                <img class="medal__img" src="/static/images/medal.png">
+                                <div class="medal__text text-small">За битву при Сомме</div>
+                            </div>
+                            <div class="medal">
+                                <img class="medal__img" src="/static/images/medal.png">
+                                <div class="medal__text text-small">Имени Гудкова</div>
+                            </div>
+                        </div>
+                        <div class="flexbox__item">
+                            <div class="medal">
+                                <img class="medal__img" src="/static/images/medal.png">
+                                <div class="medal__text text-small">Кацевская</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
