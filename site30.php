b43bdf90c2622e284d4a1a7e6fd8e5e8b1f04fd8
mirtlbu <bubuzoglo@gmail.com>
2016-03-10 18:17:34 +0300

    site3 dashboard

---
+++ site30.php
@@ -0,0 +1,37 @@
+<div class="content events">
+    <div class="dashboard">
+        <div class="container flexbox">
+            <div class="flexbox__item">
+                <div class="dashboard__logo"></div>
+                <div class="dashboard__text">
+                    Независимый депутат зависит только от своих избирателей. Он отчитывается перед ними,
+                    а не перед чиновниками из администрации президента.
+                    Его не&nbsp;купить и&nbsp;не&nbsp;запугать. Независимых депутатов почти нет в&nbsp;Думе, но&nbsp;сейчас мы можем это&nbsp;изменить.
+                </div>
+            </div>
+            <div class="flexbox__item">
+                <div class="donate">
+                    <div class="donate__text">Кампания работает только на&nbsp;ваши пожертвования.
+                        Без денег газеты не&nbsp;напечатаешь, грузчиков, не наймешь и&nbsp;штаб не&nbsp;арендуешь.
+                    </div>
+                    <button class="donate__button button button--red" type="button">Помочь деньгми</button>
+                    <div class="counter">
+                        <div class="counter__item">
+                            <span class="counter__num">200</span>
+                            <span class="counter__text">тыс. ₽ </span>
+                            <div class="counter__text">пришло <br>за неделю</div>
+                        </div>
+                        <div class="counter__item">
+                            <div class="counter__num">/</div>
+                        </div>
+                        <div class="counter__item">
+                            <span class="counter__num">700</span>
+                            <span class="counter__text">тыс. ₽ </span>
+                            <div class="counter__text">необходимо</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
