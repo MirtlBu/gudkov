6f78f3f2630ac16b1f22cc2bc9fd8972a5eec214
mirtlbu <bubuzoglo@gmail.com>
2016-03-13 02:10:01 +0300

    popup for site32

---
+++ modal6.php
@@ -0,0 +1,10 @@
+<div class="modal-wrap modal-wrap--active">
+    <div class="modal modal6">
+        <div class="modal__title">
+            <div class="text-bigger">Спасибо, что записались!</div>
+        </div>
+        <div>Организатор мероприятия Екатерина Преснецова, её телефон вот,
+            напишите ей в <a href="" class="text-link">Telegram</a> чтобы обсудить детали</div>
+        <div class="icon-close"></div>
+    </div>
+</div>
