6924862d76ec63dc86a3d8589e0b37e9098380de
mirtlbu <bubuzoglo@gmail.com>
2016-03-14 20:16:10 +0300

    fix social

---
+++ modal7.php
@@ -0,0 +1,13 @@
+<div class="modal-wrap modal-wrap--active">
+    <div class="modal modal6">
+        <div class="modal__title">
+            <div class="text-bigger">Спасибо, что записались!</div>
+        </div>
+        <div class="flexbox">
+            <div class="flexbox__item"></div>
+            <div class="flexbox__item"></div>
+            <div class="flexbox__item"></div>
+        </div>
+        <div class="icon-close"></div>
+    </div>
+</div>
