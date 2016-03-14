8cbb5c3738ded9884c32a8cd0f4bf33d8460a628
mirtlbu <bubuzoglo@gmail.com>
2016-03-14 22:18:39 +0300

    modal7 - popup 3.5

--- modal7.php
+++ modal7.php
@@ -1,12 +1,93 @@
-<div class="modal-wrap modal-wrap--active">
-    <div class="modal modal6">
+<div class="modal-wrap modal-wrap--active" style="display: block">
+    <div class="modal enrolled">
         <div class="modal__title">
-            <div class="text-bigger">Спасибо, что записались!</div>
+            <span class="text-bigger">Пикет в Щукино</span>
+            (записались <span>35 из 100</span>)
         </div>
-        <div class="flexbox">
-            <div class="flexbox__item"></div>
-            <div class="flexbox__item"></div>
-            <div class="flexbox__item"></div>
+        <div class="flexbox enrolled__item">
+            <div class="flexbox__item">
+                <div class="enrolled__info">
+                    <div class="enrolled__person">
+                        <span class="enrolled__num text-small">1</span>
+                        <span class="enrolled__name text-big">Валера Шмардин</span>
+                    </div>
+                    <span class="enrolled__city text-small">г. Москва</span>
+                    <span class="enrolled__age text-small">Возраст: <span>19</span></span>
+                </div>
+            </div>
+            <div class="flexbox__item">
+                <div class="enrolled__contacts">
+                    <div class="enrolled__phone">+7 904 345-66-78</div>
+                    <div class="enrolled__social">
+                        <span class="text-link email">valera99@mail.ru</span><br>
+                        <span class="text-link twitter">twitter</span>
+                        <span class="text-link odnoklassniki">odnoklassniki</span>
+                        <span class="text-link vkontakte">vkontakte</span>
+                        <div class="unsocial">Не ведёт соцсети</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flexbox__item">
+                <div class="enrolled__quote text-small">
+                    Вы здесь потому, что вы за все хорошее против всего плохого. Даже один независимый депутат в Думе может многое.
+                </div>
+            </div>
+        </div>
+        <div class="flexbox enrolled__item">
+            <div class="flexbox__item">
+                <div class="enrolled__info">
+                    <div class="enrolled__person">
+                        <span class="enrolled__num text-small">2</span>
+                        <span class="enrolled__name text-big">Александр Петров-Водкин</span>
+                    </div>
+                    <span class="enrolled__city text-small">г. Москва</span>
+                    <span class="enrolled__age text-small">Возраст: <span>23</span></span>
+                </div>
+            </div>
+            <div class="flexbox__item">
+                <div class="enrolled__contacts">
+                    <div class="enrolled__phone">+7 904 345-66-78</div>
+                    <div class="enrolled__social">
+                        <span class="text-link email">valera99@mail.ru</span><br>
+                        <div class="unsocial unsocial--active">Не ведёт соцсети</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flexbox__item">
+                <div class="enrolled__quote text-small">
+                    Это правда важно — независимые кандидаты проигрывают выборы потому, что наши избиратели не приходят на участок
+                    (а избиратель кандидата от власти будет приведён начальником). Позвольте нам напомнить вам о выборах перед ними.
+                </div>
+            </div>
+        </div>
+        <div class="flexbox enrolled__item">
+            <div class="flexbox__item">
+                <div class="enrolled__info">
+                    <div class="enrolled__person">
+                        <span class="enrolled__num text-small">3</span>
+                        <span class="enrolled__name text-big">Николай Преображенский</span>
+                    </div>
+                    <span class="enrolled__city text-small">г. Санкт-Петербург</span>
+                    <span class="enrolled__age text-small">Возраст: <span>21</span></span>
+                </div>
+            </div>
+            <div class="flexbox__item">
+                <div class="enrolled__contacts">
+                    <div class="enrolled__phone">+7 904 345-66-78</div>
+                    <div class="enrolled__social">
+                        <span class="text-link email">valera99@mail.ru</span><br>
+                        <span class="text-link twitter">twitter</span>
+                        <span class="text-link vkontakte">vkontakte</span>
+                        <div class="unsocial">Не ведёт соцсети</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flexbox__item">
+                <div class="enrolled__quote text-small">
+                    Мы можем с вами ситуацию переломить. Кандидата сможем избрать, если вы нам поможете привести на выборы
+                    как можно больше своих соседей.
+                </div>
+            </div>
         </div>
         <div class="icon-close"></div>
     </div>
