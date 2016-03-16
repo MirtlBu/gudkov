56072f6f9024ac688abc8f9873fe231b0e556b81
mirtlbu <bubuzoglo@gmail.com>
2016-03-16 14:40:31 +0300

    modal8 and icon +1 in pomoshnik_tasklist

---
+++ modal8.php
@@ -0,0 +1,18 @@
+<div class="modal-wrap modal-wrap--active">
+    <div class="modal cheerup">
+        <div class="flexbox">
+            <div class="flexbox__item cheerup__coordinator">
+                <div class="cheerup__img">
+                    <img src="/static/images/userpic.jpg">
+                </div>
+                <div class="cheerup__name">Алексей Шпановский</div>
+                <div class="cheerup__contact text-link text-small">Телеграм</div>
+            </div>
+            <div class="flexbox__item">
+                <div class="cheerup__dash"></div>
+            </div>
+            <div class="cheerup__quote text-big">И опыт, сын ошибок трудных! Значит, завтра будет легче.</div>
+        </div>
+        <div class="icon-close"></div>
+    </div>
+</div>
