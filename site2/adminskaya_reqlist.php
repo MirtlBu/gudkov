6b9b12ae93ba6c457376cf32281401210afc5e9d
mirtlbu <bubuzoglo@gmail.com>
2016-03-21 08:49:31 +0300

    req list page

---
+++ site2/adminskaya_reqlist.php
@@ -0,0 +1,124 @@
+<div class="content">
+    <div class="dashboard">
+        <div class="container flexbox flexbox--verticalbottom">
+            <div class="flexbox__item">
+                <div class="flexbox__container">
+                    <div class="dashboard__title">Админская</div>
+                    <div class="dashboard__tablo">
+                        <div class="dashboard__tasks">
+                            <a href="" class="text-link">Список заданий</a>
+                        </div>
+                        <div class="dashboard__errors">
+                            <a href="" class="text-link">Ошибки пользователей</a>
+                            <span>+34</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flexbox__item">
+                <div class="flexbox__container">
+                    <a href="" class="button button--blue button--small">+1 голос к победе</a>
+                    <a href="" class="button button--blue button--small">Добавить координатора</a>
+                    <div>Добавить шефа</div>
+                    <a href="" class="button button--blue button--small button--short">Из заявок</a>
+                    <a href="" class="button button--blue button--small button--short">Нового</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="reqlist">
+            <div class="">
+                <div class="text-bigger main-title">Список заявок в помощники</div>
+            </div>
+            <div class="table">
+                <div class="table__header flexbox">
+                    <div class="text-small flexbox__item">
+                        <div class="text-small flexbox__item filter">
+                            <div class="filter__title">Все заявки</div>
+                            <select class="filter__dropdown">
+                                <option class="filter__option">Сначала новые заявки</option>
+                                <option class="filter__option">Сначала старые заявки</option>
+                                <option class="filter__option">Все заявки</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="text-small flexbox__item">Соцсети</div>
+                    <div class="text-small flexbox__item">Адрес</div>
+                </div>
+                <div class="table__row flexbox listitem">
+                    <div class="flexbox__item">
+                        <div class="reqlist__date text-small text-light">15 марта</div>
+                        <div class="reqlist__person">
+                            <div class="reqlist__name">
+                                <a href="" class=" text-link text-big">Алексей Шпановский</a>
+                            </div>
+                            <div class="reqlist__phone text-small">+7 904 335-66-45</div>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="text-small">
+                            <a href="" class="text-link">Telegram</a>
+                        </div>
+                        <div class="text-small">
+                            <a href="" class="text-link">Facebook</a>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        пр.Маршала Жукова, д.33
+                    </div>
+                </div>
+                <div class="table__row flexbox listitem">
+                    <div class="flexbox__item">
+                        <div class="reqlist__date text-small text-light">15 марта</div>
+                        <div class="reqlist__person">
+                            <div class="reqlist__name">
+                                <a href="" class=" text-link text-big">Константин Константинопольский</a>
+                            </div>
+                            <div class="reqlist__phone text-small">+7 904 335-66-45</div>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="text-small">
+                            <a href="" class="text-link">Telegram</a>
+                        </div>
+                        <div class="text-small">
+                            <a href="" class="text-link">Facebook</a>
+                        </div>
+                        <div class="text-small">
+                            <a href="" class="text-link">Одноклассники</a>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        пр.маршала Рокоссовского, пр.5, стр.1, кв.45
+                    </div>
+                </div>
+                <div class="table__row flexbox listitem">
+                    <div class="flexbox__item">
+                        <div class="reqlist__date text-small text-light">15 марта</div>
+                        <div class="reqlist__person">
+                            <div class="reqlist__name">
+                                <a href="" class=" text-link text-big">Валера Кизякин</a>
+                            </div>
+                            <div class="reqlist__phone text-small">+7 904 335-66-45</div>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="text-small">
+                            <a href="" class="text-link">Telegram</a>
+                        </div>
+                        <div class="text-small">
+                            <a href="" class="text-link">Facebook</a>
+                        </div>
+                        <div class="text-small">
+                            <a href="" class="text-link">Одноклассники</a>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        пр.маршала Рокоссовского, пр.5, стр.1, кв.45
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
