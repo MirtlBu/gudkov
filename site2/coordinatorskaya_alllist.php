51ec0018e01348061abfd0c8f615cb6e3fc52a91
mirtlbu <bubuzoglo@gmail.com>
2016-03-25 23:57:17 +0300

    coordinatorskaya_alllist

---
+++ site2/coordinatorskaya_alllist.php
@@ -0,0 +1,178 @@
+<div class="content coordinatorskaya">
+    <div class="dashboard">
+        <div class="container flexbox">
+            <div class="flexbox__item">
+                <div class="flexbox__container">
+                    <div class="dashboard__user">
+                        <div class="dashboard__img">
+                            <img class="dashboard__img" src="/static/images/ron.jpg">
+                        </div>
+                        <div class="dashboard__title">Константин</div>
+                    </div>
+                    <div class="dashboard__tablo">
+                        <div class="">Валера, как прошёл день?</div>
+                        <div class="dashboard__rate">
+                            <button class="button button--small button--red">Плохо</button>
+                            <button class="button button--small button--blue">Всё по плану</button>
+                            <button class="button button--small button--green">Отлично</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flexbox__item">
+                <div class="flexbox__container">
+                    <a href="" class="button button--blue button--small" type="button">+1 голос к победе</a>
+                    <div>Добавить шефа</div>
+                    <a href="" class="button button--blue button--small button--short">Из заявок</a>
+                    <a href="" class="button button--blue button--small button--short">Нового</a>
+                    <div class="dashboard__row dashboard__bold">УИКи:</div>
+                    <div class="flexbox">
+                        <div class="flexbox__item">
+                            <div class="dashboard__row">23534667</div>
+                            <div class="dashboard__row">23534667</div>
+                        </div>
+                        <div class="flexbox__item">
+                            <div class="dashboard__row">23534667</div>
+                            <div class="dashboard__row">23534667</div>
+                        </div>
+                        <div class="flexbox__item">
+                            <div class="dashboard__row">23534667</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="coord reqlist">
+            <div class="text-bigger main-title">Список всех помощников</div>
+            <div class="table">
+                <div class="table__header flexbox">
+                    <div class="text-small flexbox__item">По дате</div>
+                    <div class="text-small flexbox__item">По алфавиту</div>
+                    <div class="text-small flexbox__item"></div>
+                    <div class="text-small flexbox__item">По УИКУ</div>
+                    <div class="text-small flexbox__item">По кластеру</div>
+                    <div class="text-small flexbox__item">По заданиям</div>
+                </div>
+                <div class="table__row flexbox listitem">
+                    <div class="flexbox__item">
+                        <div class="reqlist__date text-small text-light">15 марта</div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="listitem__img">
+                            <img src="../../static/images/userpic.jpg">
+                        </div>
+                        <div class="reqlist__person">
+                            <div class="reqlist__name">
+                                <a href="" class="text-big">Алексей Шпановский</a>
+                            </div>
+                            <div class="reqlist__phone text-small">+7 904 335-66-45</div>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="text-small">
+                            <a href="" class="text-link">Telegram</a>
+                        </div>
+                        <div class="text-small">
+                            <a href="" class="text-link">Facebook</a>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="reqlist__comment">ул. Ленина, дом 16, кв. 45</div>
+                        <div class="flexbox flexbox--double">
+                            <div class="flexbox__item text-small">
+                                <div>345346</div>
+                                <div>5346</div>
+                            </div>
+                            <div class="flexbox__item text-small">
+                                <div>34</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div>2</div>
+                    </div>
+                </div>
+                <div class="table__row table__row--removed flexbox listitem">
+                    <div class="flexbox__item">
+                        <div class="reqlist__date text-small text-light">15 марта</div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="listitem__img">
+                            <img src="../../static/images/userpic.jpg">
+                        </div>
+                        <div class="reqlist__person">
+                            <div class="reqlist__name">
+                                <a href="" class="text-big">Алексей Шпановский</a>
+                            </div>
+                            <div class="reqlist__phone text-small">+7 904 335-66-45</div>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="text-small">
+                            <a href="" class="text-link">Telegram</a>
+                        </div>
+                        <div class="text-small">
+                            <a href="" class="text-link">Facebook</a>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="reqlist__comment">Текст причины удаления человека координатором</div>
+                        <div class="flexbox flexbox--double">
+                            <div class="flexbox__item text-small">
+                                <div>345346</div>
+                                <div>5346</div>
+                            </div>
+                            <div class="flexbox__item text-small">
+                                <div>34</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div>2</div>
+                    </div>
+                </div>
+                                <div class="table__row flexbox listitem">
+                    <div class="flexbox__item">
+                        <div class="reqlist__date text-small text-light">15 марта</div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="listitem__img">
+                            <img src="../../static/images/userpic.jpg">
+                        </div>
+                        <div class="reqlist__person">
+                            <div class="reqlist__name">
+                                <a href="" class="text-big">Алексей Шпановский</a>
+                            </div>
+                            <div class="reqlist__phone text-small">+7 904 335-66-45</div>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="text-small">
+                            <a href="" class="text-link">Telegram</a>
+                        </div>
+                        <div class="text-small">
+                            <a href="" class="text-link">Facebook</a>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div class="reqlist__comment">ул. Ленина, дом 16, кв. 45</div>
+                        <div class="flexbox flexbox--double">
+                            <div class="flexbox__item text-small">
+                                <div>345346</div>
+                                <div>5346</div>
+                            </div>
+                            <div class="flexbox__item text-small">
+                                <div>34</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flexbox__item">
+                        <div>2</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
