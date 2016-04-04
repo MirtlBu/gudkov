26bcafff51681f74cc3fc57d53a7bb0cc15ba9b1
mirtlbu <bubuzoglo@gmail.com>
2016-04-04 22:26:15 +0300

    modal10 for calls

---
+++ site4/modal10.php
@@ -0,0 +1,20 @@
+<div class="modal-wrap modal-wrap--active" style="display: block">
+    <form class="modal modal10">
+        <div class="form__row">
+            <label for="phone" class="form__label">Телефон</label>
+            <input id="phone" name="phone" class="form__input" type="text" required>
+        </div>
+        <div class="form__row form__row--nomargin">
+            <label for="password" class="form__label">Пароль</label>
+            <input id="password" name="password" class="form__input" type="password" required>
+        </div>
+        <div class="form__row ">
+            <div class="text-link text-small">Забыл пароль</div>
+        </div>
+        <div class="form__row form__row--button">
+            <label class="form__label"></label>
+            <button class="button button--blue" type="submit">Войти</button>
+        </div>
+        <div class="icon-close"></div>
+    </form>
+</div>
