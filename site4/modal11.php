79c86a52fc7b6b5f8560d38e10a2462f841590af
mirtlbu <bubuzoglo@gmail.com>
2016-04-04 22:35:29 +0300

    modal11 for calls

---
+++ site4/modal11.php
@@ -0,0 +1,14 @@
+<div class="modal-wrap modal-wrap--active" style="display: block">
+    <form class="modal modal11">
+        <div class="form__row">
+            <div>Введите телефон, указанный при регистрации</div>
+        </div>
+        <div class="form__row">
+            <input id="phone" name="phone" class="form__input" type="text" required>
+        </div>
+        <div class="form__row">
+            <button class="button button--blue" type="submit">Получить пароль</button>
+        </div>
+        <div class="icon-close"></div>
+    </form>
+</div>
