446ef67856f1edbd2eb78852b8e428343a46e5e8
mirtlbu <bubuzoglo@gmail.com>
2016-04-04 22:49:40 +0300

    oops, modal12 and modal13

---
+++ site4/modal12.php
@@ -0,0 +1,8 @@
+<div class="modal-wrap modal-wrap--active" style="display: block">
+    <form class="modal modal12">
+        <div class="form__row">
+            <div>Выслали вам пароль на указанный при регистрации эл. адрес</div>
+        </div>
+        <div class="icon-close"></div>
+    </form>
+</div>
---
+++ site4/modal13.php
@@ -0,0 +1,13 @@
+<div class="modal-wrap modal-wrap--active" style="display: block">
+    <form class="modal modal13">
+        <div class="centered">
+            <div class="form__row">
+                <div>Такой номер ещё не зарегистирован</div>
+            </div>
+            <div class="form__row">
+                <button class="button button--blue" type="submit">Присоединиться к команде</button>
+            </div>
+        </div>
+        <div class="icon-close"></div>
+    </form>
+</div>
