3e15a26565f6a75a05e25f2480a43d1b962760a3
mirtlbu <bubuzoglo@gmail.com>
2016-04-08 15:03:27 +0300

    modal4 style

--- site2/modal4.php
+++ site2/modal4.php
@@ -4,14 +4,16 @@
             <label for="phone" class="form__label">Телефон</label>
             <input id="phone" name="phone" class="form__input auth_phone" value="+7" type="text" required>
         </div>
-        <div class="form__row">
+        <div class="form__row form__row--nomargin">
             <label for="password" class="form__label">Пароль</label>
             <input id="password" name="password" class="form__input auth_password" type="password" required>
+        </div>
+        <div class="form__row ">
+            <a class="text-link text-small" onClick="$('.authPopup').fadeOut(); $('.restorePopup').fadeIn(500);">Забыли пароль?</a>
         </div>
         <div class="form__row form__row--button">
             <label class="form__label"></label>
             <button class="button button--blue" type="submit" onClick="">Войти</button>
-			<a style="color: #0195ff;vertical-align: bottom;display: inline;cursor: pointer;font-size: 14px;margin-left: 10px;" onClick="$('.authPopup').fadeOut(); $('.restorePopup').fadeIn(500);">Забыли пароль?</a>
         </div>
         <div class="icon-close"></div>
     </form>
